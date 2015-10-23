a5c7a2c9aa3a3027ea6cfd925a304287365ec691
Eric M. David <ericdavid365@gmail.com>
2015-10-23 11:29:46 -0500

    test

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -7,4 +7,12 @@ describe Array do
 			expect((array.rand_array).select { |x| x == 0 }.size).to eq 0
 		end
 	end
+
+	context "#number_matches" do
+		it "compares two arrays and returns identical elements in same index" do
+			array = [0,0,1]
+			array2 = [0,3,1]
+			expect(array.number_matches(array2)).to eq 2
+		end
+	end
 end
